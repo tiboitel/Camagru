9fda9ea0934e26ca674c23f25aaee589baca1b5b
tiboitel <tiboitel@student.42.fr>
2025-05-05 18:03:45 +0200

    wip(docker): working on setting up environements variables on containers.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -f /etc/msmtprc.template ]; then
+    echo "Template file not found!"
+    exit 1
+fi
+
+envsubst < /etc/msmtprc.template > /etc/msmtprc
+
+chmod 644 /etc/msmtprc
+chown root:root /etc/msmtprc
+
+php-fpm --daemonize
+set -e 
+
--- docker/nginx.Dockerfile
+++ docker/nginx.Dockerfile
@@ -1,6 +1,5 @@
 FROM nginx:latest
 
-# Copy configuration and fix permissions
-COPY nginx.conf /etc/nginx/conf.d/nginx.conf
+COPY ../docker/nginx.conf /etc/nginx/conf.d/nginx.conf
 
 EXPOSE 80

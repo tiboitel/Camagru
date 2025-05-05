4507526cfb5a81c6b8a8ab03b11468b2546cf11b
tiboitel <tiboitel@student.42.fr>
2025-05-05 18:29:50 +0200

    feat(docker): Set up environment variables for Docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -f /etc/msmtprc.template ]; then
     echo "Template file not found!"
     exit 1
@@ -10,6 +12,4 @@ envsubst < /etc/msmtprc.template > /etc/msmtprc
 chmod 644 /etc/msmtprc
 chown root:root /etc/msmtprc
 
-php-fpm --daemonize
-set -e 
-
+exec php-fpm
